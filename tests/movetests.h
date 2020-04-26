36acae337718ff5906b4051c9eac614b891bee67
matt bimpson <m.bimpson@gmail.com>
2020-04-26 10:39:08 -0500

    add another test

--- tests/movetests.h
+++ tests/movetests.h
@@ -5,11 +5,21 @@
 
 class MoveTests : public CxxTest::TestSuite
 {
-    Moves _moves;
+    
     public: void test_getColumns_Returns_Columns(void)
     {
+        Moves _moves;
         vector<vector<char>> columns = _moves.getColumns();
         TS_ASSERT(columns.size() == 7);
         TS_ASSERT(columns[0].size() == 6);
+    }
+
+    public: void test_placeToken_Returns_False(void)
+    {
+        Moves _moves;
+        vector<vector<char>> columns = _moves.getColumns();
+        bool result = false;
+        result = _moves.placeToken(0, true);
+        TS_ASSERT(result == false);
     }
 };
